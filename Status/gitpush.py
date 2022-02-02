b4fe9d03bd628fc60897ebbf67ae12a454493305
Echo <echo@escape.ac.cn>
2022-02-02 23:14:22 +0800

    2022-02-02

--- Status/gitpush.py
+++ Status/gitpush.py
@@ -4,14 +4,17 @@ def git_push():
     p = subprocess.Popen('python status.py', shell=True, 
                  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     _ = p.wait()
+    print(1)
     p = subprocess.Popen('git add .', shell=True, 
                  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     _ = p.wait()
+    print(2)
     today_date = datetime.datetime.today().strftime('%Y-%m-%d')
     commit_content = 'git commit -m "{}"'.format(today_date)
     p = subprocess.Popen(commit_content, shell=True, 
                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     _ = p.wait()
+    print(3)
     p = subprocess.Popen('git push', shell=True, 
                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out, _  = p.communicate()
